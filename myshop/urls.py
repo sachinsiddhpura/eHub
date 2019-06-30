836351680b6906a385e843ecaca5f8fa913956c5
Sachin Siddhpura <sachinsids007@gmail.com>
2019-06-30 11:22:22 +0530

    Update urls.py

--- myshop/urls.py
+++ myshop/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
